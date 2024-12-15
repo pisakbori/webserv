2a3bf99b75463ef537f35f9a1e55691ebdb57db9
Marian Kijewski <mkijewsk@2-I-5.42heilbronn.de>
2024-12-15 20:00:36 +0100

    Implement parsing of listen directive

--- Server.cpp
+++ Server.cpp
@@ -6,28 +6,65 @@
 /*   By: mkijewsk <mkijewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 22:03:33 by mkijewsk          #+#    #+#             */
-/*   Updated: 2024/12/14 23:18:52 by mkijewsk         ###   ########.fr       */
+/*   Updated: 2024/12/15 19:58:20 by mkijewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
 /*
-	default server configuration
-	only listen directive is required
+ * default server configuration
+ * only listen directive is required
 */
 Server::Server() :
 	host("0.0.0.0"),
-	ports(new unsigned short(80)),
-	server_name(new std::string()),
-	is_default(true), // should be true if this is the first server in the conf
+	port(80),
+	server_name(new std::string())
+	// is_default(true), // should be true if this is the first server in the conf
 	// error_page(NULL),
-	client_max_body_size(1'000'000)
+	// client_max_body_size(1'000'000)
 {
 }
 
 Server::~Server()
 {
-	delete ports;
 	delete server_name;
+}
+
+void	Server::parse_listen(std::string directive)
+{
+	std::string		argument;
+
+	if (directive.rfind("listen ") != 0 || directive.back() != ';')
+		std::cerr << "Invalid listen directive" << std::endl;
+	argument = directive.substr(directive.find(' ') + 1);
+	argument = argument.substr(0, argument.size() - 1);
+	const size_t	colon_pos = argument.rfind(':');
+	if (colon_pos != std::string::npos &&
+		(argument.find(']') < colon_pos ||
+		argument.find(']') == std::string::npos))
+	{
+		host = argument.substr(0, colon_pos);
+		port = std::stoi(argument.substr(colon_pos + 1));
+	}
+	else if (argument.find_first_not_of("0123456789") == std::string::npos)
+		port = std::stoi(argument);
+	else
+		host = argument;
+}
+
+std::string		Server::get_host( void ) const
+{
+	return host;
+}
+
+unsigned short	Server::get_port( void ) const
+{
+	return port;
+}
+
+std::ostream&	operator<<(std::ostream& os, const Server& serv)
+{
+	os << serv.get_host() << ":" << serv.get_port();
+	return os;
 }
--- Server.hpp
+++ Server.hpp
@@ -6,7 +6,7 @@
 /*   By: mkijewsk <mkijewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 15:02:07 by mkijewsk          #+#    #+#             */
-/*   Updated: 2024/12/14 23:23:58 by mkijewsk         ###   ########.fr       */
+/*   Updated: 2024/12/15 18:26:30 by mkijewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@
 
 // # include <map>
 # include <string>
+# include <iostream>
 # include <arpa/inet.h>
 # include <sys/socket.h>
 
 /*
-Server class stores information regarding server directive in the configuration
-It's inspired by nginx directives: 
-https://nginx.org/en/docs/http/ngx_http_core_module.html
-#TODO
-- [ ] (?) Inherit from the http directive
-- [ ] Implement location member: vvv
-https://www.digitalocean.com/community/tutorials/understanding-nginx-server-and-location-block-selection-algorithms
-- [ ] Implement error_page member
-- [ ] If cannot listen on default port 80 try 8080
+ * Server class stores information regarding server directive in the .conf
+ * It's inspired by nginx directives: 
+ * https://nginx.org/en/docs/http/ngx_http_core_module.html
+ * #TODO
+ * - [ ] (?) Inherit from the http directive
+ * - [ ] Implement location member: vvv
+ * https://www.digitalocean.com/community/tutorials/understanding-nginx-server-and-location-block-selection-algorithms
+ * - [ ] Implement error_page member
+ * - [ ] If cannot listen on default port 80 try 8080
 */
 
 class Server
@@ -36,11 +37,11 @@ class Server
 	private:
 
 		std::string					host;
-		unsigned short				*ports;
+		unsigned short				port;
 		std::string					*server_name;
-		bool						is_default;
+		// bool						is_default;
 		// std::map<int, std::string>	error_page;
-		size_t						client_max_body_size;
+		// size_t						client_max_body_size;
 		// location
 
 	public:
@@ -48,7 +49,12 @@ class Server
 		Server();
 		~Server();
 
+		void			parse_listen(std::string directive);
+		std::string		get_host( void ) const;
+		unsigned short	get_port( void ) const;
+
 
 };
+std::ostream&	operator<<(std::ostream& os, const Server& serv);
 
 #endif
