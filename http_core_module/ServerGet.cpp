9156d59cc999ceb571068d74046263a36fd63e76
Marian Kijewski <mkijewsk@3-F-12.42heilbronn.de>
2025-01-13 18:59:28 +0100

    Introduce location matching algorithm

--- http_core_module/ServerGet.cpp
+++ http_core_module/ServerGet.cpp
@@ -25,3 +25,26 @@ size_t						Server::get_client_max_body_size(void) const
 {
 	return client_max_body_size;
 }
+
+const Location &			Server::get_location(std::string requested_uri) const
+{
+	unsigned int	longest_match;
+	std::string 	location_uri;
+	int				position_match;
+
+	longest_match = 0;
+	position_match = -1;
+	for (size_t i = 0; i < location.size(); ++i)
+	{
+		location_uri = location.at(i).get_uri();
+		if (requested_uri.compare(0, location_uri.length(), location_uri) == 0 &&
+			longest_match < location_uri.length())
+			{
+				position_match = i;
+				longest_match = location_uri.length();
+			}
+	}
+	if (position_match == -1)
+		throw HttpError("Oh no! " + str + " not found.", 404);
+	return location.at(position_match);
+}
